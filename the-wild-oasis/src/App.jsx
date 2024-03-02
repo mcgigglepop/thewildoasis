a68a1c7569da86f12fffce2bb6024c596c344909
cristopher ontiveros <christoph.alano@gmail.com>
2024-03-01 21:21:48 -0600

    more styles

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,15 +1,7 @@
 import styled from "styled-components";
 import GlobalStyle from "./styles/GlobalStyles";
 
-// eslint-disable-next-line no-unused-vars
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
-
 const StyledApp = styled.div`
-  background-color: orangered;
   padding: 20px;
 `;
 
