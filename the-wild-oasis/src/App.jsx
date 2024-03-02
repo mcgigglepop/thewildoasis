f3f8723b53123908fdd6c1662cb6633c0a4de522
cristopher ontiveros <christoph.alano@gmail.com>
2024-03-01 20:40:19 -0600

    adding styles

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,5 +1,39 @@
+import styled from "styled-components";
+
+// eslint-disable-next-line no-unused-vars
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+// eslint-disable-next-line no-unused-vars
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`;
+
+// eslint-disable-next-line no-unused-vars
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello world</div>
+  return <StyledApp>Hello world</StyledApp>
 }
 
 export default App;
