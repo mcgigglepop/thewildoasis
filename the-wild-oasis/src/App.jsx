fb249bf92e7ecf44cf0d95d86a24c141085d3653
cristopher ontiveros <christoph.alano@gmail.com>
2024-03-01 20:57:29 -0600

    added more styles

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyles";
 
 // eslint-disable-next-line no-unused-vars
 const H1 = styled.h1`
@@ -7,33 +8,18 @@ const H1 = styled.h1`
   background-color: yellow;
 `;
 
-// eslint-disable-next-line no-unused-vars
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-`;
-
-// eslint-disable-next-line no-unused-vars
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
-
 const StyledApp = styled.div`
   background-color: orangered;
   padding: 20px;
 `;
 
 function App() {
-  return <StyledApp>Hello world</StyledApp>
+  return (
+    <>
+      <GlobalStyle />
+      <StyledApp>Hello world</StyledApp>
+    </>
+  );
 }
 
 export default App;
